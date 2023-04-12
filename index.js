c15f2d7106fb9e0e553aa4955b595aa81f689cdd
SANJAY0115 <sanjaysanju0115@gmail.com>
2023-04-12 16:56:23 +0530

    final

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import { studentsRouter } from "./routes/student.js";
-// import { userRouter } from "./routes/users.js";
+import { userRouter } from "./routes/users.js";
 import cors from "cors"
 
 // env configurations
@@ -14,7 +14,7 @@ app.use(express.json()); // middleware tells server to use json
 app.use(cors()) //cors function which makes 2 different ports to speak frontend-3000 , backend-9000
  
 app.use("/students",studentsRouter)
-// app.use("/users",userRouter)
+app.use("/users",userRouter)
 // default url: users , router url : signup
 
 // export var ObjectId = obj.ObjectId      // object id for put and post in student.js
